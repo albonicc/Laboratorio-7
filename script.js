49b90169e536876813654478116f86621e17a4ed
albonicc <nikygam@gmail.com>
2021-07-16 23:41:55 -0500

    It's almost complete

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const charactersButtonsDiv = document.querySelector('#characters-buttons');
 const charactersDiv = document.querySelector('#characters');
 const API_KEY = 'WVhsJQJWD74sFtgKBR0iwCYlMlVQs78H';
 // const URL = "https://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=PU9MVKA0tF1Zuu50yJAY3mumUmTGZxy1&limit=10";
-const spongebobCharacters = ['Spongebob', 'Patrick Star', 'Squidward', 'Mr Krabs', 
+const spongebobCharacters = ['Spongebob Squarepants', 'Patrick Star', 'Squidward', 'Mr Krabs', 
                             'Plankton', 'Sandy Cheeks', 'Larry the lobster', 
                             'Mermaid Man', 'Barnacle Boy'];
 
@@ -13,19 +13,30 @@ const getGIFs = async (character) => {
     return characterGIFs;
 }
 
+const changeDataState = (element) => {
+    if (element.getAttribute('data-state') === 'still') {
+        element.setAttribute('data-state', 'animate')
+        element.setAttribute('src', element.getAttribute('data-animate'))  
+    } else {
+        element.setAttribute('data-state', 'still')
+        element.setAttribute('src', element.getAttribute('data-still'))   
+    }
+}
+
 const addCharacterGIFs = async (character) => {
     const characterGIFs = await getGIFs(character);
     // characterGIFs.data.forEach(item => console.log(item))
     characterGIFs.data.forEach(item => {
         charactersDiv.innerHTML += `
                                     <div class="gif-item">
-                                        <p>${item.rating}</p>
+                                        <p>Rating: ${item.rating}</p>
                                         <img 
                                             src="${item.images.fixed_height_still.url}"
                                             class="character-image"
                                             data-still="${item.images.fixed_height_still.url}"
-                                            data-animte="${item.images.fixed_height.url}"
+                                            data-animate="${item.images.fixed_height.url}"
                                             data-state="still"
+                                            onclick="changeDataState(this)"
                                         >
                                     </div>
         `
