7c70fc80c937e9dd1260b39d740ca60b0281435e
albonicc <nikygam@gmail.com>
2021-07-16 23:17:02 -0500

    Almost finished

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
+const charactersButtonsDiv = document.querySelector('#characters-buttons');
 const charactersDiv = document.querySelector('#characters');
 const API_KEY = 'WVhsJQJWD74sFtgKBR0iwCYlMlVQs78H';
 // const URL = "https://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=PU9MVKA0tF1Zuu50yJAY3mumUmTGZxy1&limit=10";
-const spongebobCharacters = ['Spongebob', 'Patrick Star', 'Squidward', 'Mr Krabs',         'Plankton', 'Sandy Cheeks', 'Larry the lobster', 'Mermaid Man', 'Barnacle Boy'];
+const spongebobCharacters = ['Spongebob', 'Patrick Star', 'Squidward', 'Mr Krabs', 
+                            'Plankton', 'Sandy Cheeks', 'Larry the lobster', 
+                            'Mermaid Man', 'Barnacle Boy'];
 
 const getGIFs = async (character) => {
-    let URL = `https://api.giphy.com/v1/gifs/search?q=${character}&api_key=${API_KEY}&limit=10`;
+    const URL = `https://api.giphy.com/v1/gifs/search?q=${character}&api_key=${API_KEY}&limit=10`;
+    const response = await fetch(URL);
+    const characterGIFs = await response.json();
+    return characterGIFs;
+}
 
-    response = await fetch(URL);
-    console.log(response);
+const addCharacterGIFs = async (character) => {
+    const characterGIFs = await getGIFs(character);
+    // characterGIFs.data.forEach(item => console.log(item))
+    characterGIFs.data.forEach(item => {
+        charactersDiv.innerHTML += `
+                                    <div class="gif-item">
+                                        <p>${item.rating}</p>
+                                        <img 
+                                            src="${item.images.fixed_height_still.url}"
+                                            class="character-image"
+                                            data-still="${item.images.fixed_height_still.url}"
+                                            data-animte="${item.images.fixed_height.url}"
+                                            data-state="still"
+                                        >
+                                    </div>
+        `
+    })
+    
 }
 
 const getButtonValue = (characterID) => {
     const character = document.querySelector(`#${characterID}`).innerHTML;
-
-    alert(character)
+    addCharacterGIFs(character);
 }
 
 
@@ -21,5 +43,5 @@ const getButtonValue = (characterID) => {
 spongebobCharacters.forEach(character => {
     let characterID = character.split(' ').join('-').toLowerCase();
 
-    charactersDiv.innerHTML += `<button id="${characterID}" onclick="getButtonValue('${characterID}')">${character}</button>`
+    charactersButtonsDiv.innerHTML += `<button id="${characterID}" onclick="getButtonValue('${characterID}')">${character}</button>`
 })
